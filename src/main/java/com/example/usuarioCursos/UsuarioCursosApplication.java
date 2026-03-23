6b376ede16d374e8d856de08ab963e2c53b07247
eloy <eloy.elnet@gmail.com>
2026-03-23 13:14:05 +0100

    init

---
+++ src/main/java/com/example/usuarioCursos/UsuarioCursosApplication.java
@@ -0,0 +1,13 @@
+package com.example.usuarioCursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usuarioCursos/UsuarioCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usuarioCursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
